38c8a5b4ef0207b82d59ba01924177bd6b01147c
Michael M Slusarz <slusarz@curecanti.org>
2010-05-18 14:25:17 -0600

    Remove Core dependency in horde/LoginTasks
    Framework libs other than Core must contain NO horde dependencies, even
    if they are optional.
    
    For testing: unit tests in the framework libraries should only test whether
    the code contained in the library works. If we want to test things
    dependent on Horde (the application), these tests should be in Core.

---
+++ lib/Horde/Core/LoginTasks/Backend/Horde.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * This class provides the Horde specific implementation of the LoginTasks
+ * backend.
+ *
+ * Copyright 2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lgpl.html.
+ *
+ * @author   Michael Slusarz <slusarz@horde.org>
+ * @author   Gunnar Wrobel <wrobel@pardus.de>
+ * @category Horde
+ * @package  Core
+ */
+class Horde_Core_LoginTasks_Backend_Horde extends Horde_LoginTasks_Backend
+{
+    /**
+     * The Horde application that is currently active.
+     *
+     * @var string
+     */
+    private $_app;
+
+    /**
+     * Constructor.
+     *
+     * @param string $app  The currently active Horde application.
+     */
+    public function __construct($app)
+    {
+        $this->_app = $app;
+    }
+
+    /**
+     * Is the current session authenticated?
+     *
+     * @return boolean True if the user is authenticated, false otherwise.
+     */
+    public function isAuthenticated()
+    {
+        return (Horde_Auth::getAuth() !== false);
+    }
+
+    /**
+     * Retrieve a cached tasklist if it exists.
+     *
+     * @return Horde_LoginTasks_Tasklist|boolean  The cached task list or
+     *                                            false if no task list was
+     *                                            cached.
+     */
+    public function getTasklistFromCache()
+    {
+        if (isset($_SESSION['horde_logintasks'][$this->_app])) {
+            return @unserialize($_SESSION['horde_logintasks'][$this->_app]);
+        }
+        return false;
+    }
+
+    /**
+     * Store a login tasklist in the cache.
+     *
+     * @param Horde_LoginTasks_Tasklist|boolean $tasklist  The tasklist to be
+     *                                                     stored.
+     */
+    public function storeTasklistInCache($tasklist)
+    {
+        $_SESSION['horde_logintasks'][$this->_app] = serialize($tasklist);
+    }
+
+    /**
+     * Register the shutdown handler.
+     *
+     * @param array $shutdown  The shutdown function.
+     */
+    public function registerShutdown($shutdown)
+    {
+        register_shutdown_function($shutdown);
+    }
+
+    /**
+     * Get the class names of the task classes that need to be performed.
+     *
+     * @return array  An array of class names.
+     */
+    public function getTasks()
+    {
+        /* Add Horde tasks here if not yet run. */
+        $app_list = array($this->_app);
+        if (($this->_app != 'horde') &&
+            !isset($_SESSION['horde_logintasks']['horde'])) {
+            array_unshift($app_list, 'horde');
+        }
+
+        $tasks = array();
+
+        foreach ($app_list as $app) {
+            foreach (array_merge($GLOBALS['registry']->getAppDrivers($app, 'LoginTasks_SystemTask'), $GLOBALS['registry']->getAppDrivers($app, 'LoginTasks_Task')) as $val) {
+                $tasks[$val] = $app;
+            }
+        }
+
+        return $tasks;
+    }
+
+    /**
+     * Get the information about the last time the tasks were run. Array keys
+     * are app names, values are last run timestamps. Special key '_once'
+     * contains list of ONCE tasks previously run.
+     *
+     * @return array  The information about the last time the tasks were run.
+     */
+    public function getLastRun()
+    {
+        $lasttask_pref = @unserialize($GLOBALS['prefs']->getValue('last_logintasks'));
+        if (!is_array($lasttask_pref)) {
+            $lasttask_pref = array();
+        }
+
+        return $lasttask_pref;
+    }
+
+    /**
+     * Store the information about the last time the tasks were run.
+     *
+     * @param array $last  The information about the last time the tasks were
+     *                     run.
+     */
+    public function setLastRun(array $last)
+    {
+        $GLOBALS['prefs']->setValue('last_logintasks', serialize($last));
+    }
+
+    /**
+     * Mark the current time as time the login tasks were run for the last
+     * time.
+     */
+    public function markLastRun()
+    {
+        $lasttasks = $this->getLastRun();
+        $lasttasks[$this->_app] = time();
+        if (($this->_app != 'horde') &&
+            !isset($_SESSION['horde_logintasks']['horde'])) {
+            $lasttasks['horde'] = time();
+            $_SESSION['horde_logintasks']['horde'] = true;
+        }
+        $GLOBALS['prefs']->setValue('last_logintasks', serialize($lasttasks));
+    }
+
+    /**
+     * Redirect to the given URL.
+     *
+     * @param string $url  The URL to redirect to.
+     */
+    public function redirect($url)
+    {
+        header('Location: ' . $url);
+        exit;
+    }
+
+    /**
+     * Return the URL of the login tasks view.
+     *
+     * @param array $tasks  The tasks to be displayed next.
+     *
+     * @return string  The URL of the login tasks view.
+     */
+    public function getLoginTasksUrl(array $tasks = null)
+    {
+        return Horde::getServiceLink('logintasks', $this->_app);
+    }
+}
