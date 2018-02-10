7f82f5a941e2287f24c5985d803cda587dd96423
Michael J Rubinsky <mrubinsk@horde.org>
2018-02-10 17:28:28 -0500

    *sigh* and fix reversed logic.

--- lib/Horde/Core/ActiveSync/Driver.php
+++ lib/Horde/Core/ActiveSync/Driver.php
@@ -2463,8 +2463,8 @@ class Horde_Core_ActiveSync_Driver extends Horde_ActiveSync_Driver_Base
                 $res['oof'] = array(
                     'status' => Horde_ActiveSync_Request_Settings::STATUS_SUCCESS,
                     'oofstate' => ($vacation['disabled']
-                        ? Horde_ActiveSync_Request_Settings::OOF_STATE_ENABLED
-                        : Horde_ActiveSync_Request_Settings::OOF_STATE_DISABLED),
+                        ? Horde_ActiveSync_Request_Settings::OOF_STATE_DISABLED
+                        : Horde_ActiveSync_Request_Settings::OOF_STATE_ENABLED),
                     'oofmsgs' => array()
                 );
                 // If we have start/end it's a timebased vacation in EAS-speak.
