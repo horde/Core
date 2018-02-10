2a569b3861aacd9889ca848c805a5fd1db44e2eb
Michael J Rubinsky <mrubinsk@horde.org>
2018-02-10 17:24:40 -0500

    Parse error.

--- lib/Horde/Core/ActiveSync/Driver.php
+++ lib/Horde/Core/ActiveSync/Driver.php
@@ -2464,7 +2464,7 @@ class Horde_Core_ActiveSync_Driver extends Horde_ActiveSync_Driver_Base
                     'status' => Horde_ActiveSync_Request_Settings::STATUS_SUCCESS,
                     'oofstate' => ($vacation['disabled']
                         ? Horde_ActiveSync_Request_Settings::OOF_STATE_ENABLED
-                        : Horde_ActiveSync_Request_Settings::OOF_STATE_DISABLED)),
+                        : Horde_ActiveSync_Request_Settings::OOF_STATE_DISABLED),
                     'oofmsgs' => array()
                 );
                 // If we have start/end it's a timebased vacation in EAS-speak.
