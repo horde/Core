49381180a7d772d376f8f549281d8c03a0106617
Ralf Lang <lang@b1-systems.de>
2021-03-15 06:20:39 +0000

    "Add partial Unit Tests for Controller Authentication"

---
+++ test/Horde/Core/Authentication/Method/Basic.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+namespace Horde\Core\Authentication\Method;
+use \Horde\Core\Authentication\BasicMethod;
+use \PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    function testNoHeader(): void
+    {
+
+    }
+
+
+    function testFoundHeader(): void
+    {
+
+    }
+
+    function testFoundHeaderButNoBasic(): void
+    {
+
+    }
+
+}
