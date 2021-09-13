6f367cf874ff7fff79ef5121a466c9dadefb1385
Midah Pasche <midahannep@gmail.com>
2021-09-13 16:20:02 +0200

    compatibility with 'HordeAuthType' => 'NONE' in routes.php

--- src/Middleware/AppRouter.php
+++ src/Middleware/AppRouter.php
@@ -105,6 +105,10 @@ class AppRouter implements MiddlewareInterface
         $match = $this->router->match($path);
         $request = $request->withAttribute('route', $match);
 
+        // compatibility: if unset stack and HordeAuthType is 'NONE' set empty stack
+        if (!isset($match['stack']) && $match['HordeAuthType'] === 'NONE') {
+            $match['stack'] = [];
+        }
         // Stack is an array of DI keys
         // Empty array means NO more middleware besides controller
         // unset stack means DEFAULT middleware stack
