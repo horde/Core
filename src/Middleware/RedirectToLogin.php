636ea9c6169c4880dde3bf822aeb5018c8cf9ea3
boekhorstb1 <91957243+boekhorstb1@users.noreply.github.com>
2022-02-24 07:59:52 +0100

    RedirecToLogin Middleware sets 'url' parameter to be the signed current url

--- src/Middleware/RedirectToLogin.php
+++ src/Middleware/RedirectToLogin.php
@@ -14,7 +14,9 @@ use Horde_Application;
 use Horde_Controller;
 use Horde_Routes_Mapper as Router;
 use Horde_String;
+use Horde\Core\Config\State;
 use Horde;
+use Horde_Url;
 use Horde\Core\UserPassport;
 use Psr\Http\Message\ResponseFactoryInterface;
 
@@ -29,19 +31,37 @@ use Psr\Http\Message\ResponseFactoryInterface;
  */
 class RedirectToLogin implements MiddlewareInterface
 {
+    private State $conf;
     private Horde_Registry $registry;
     private ResponseFactoryInterface $responseFactory;
-    public function __construct(Horde_Registry $registry, ResponseFactoryInterface $responseFactory)
+    public function __construct(Horde_Registry $registry, ResponseFactoryInterface $responseFactory, State $conf)
     {
         $this->registry = $registry;
         $this->responseFactory = $responseFactory;
+        $this->conf = $conf;
     }
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         if ($request->getAttribute('HORDE_AUTHENTICATED_USER')) {
             return $handler->handle($request);
         }
-        $redirect = (string)Horde::Url($this->registry->getInitialPage('horde'), true);
-        return $this->responseFactory->createResponse(302)->withHeader('Location', $redirect);
+
+        $requestUrl = (string) $request->getUri();
+        $signedRequestUrl = Horde::signUrl($requestUrl);
+
+        // set baseurl: check if alternative login is set and use it as baseurl
+        $configArray = $this->conf->toArray();
+        $configArray['auth']['alternate_login'] ?? null;
+
+        if (!empty($alternateLogin)) {
+            $baseUrl = $alternateLogin;
+        } else {
+            // set baseurl: if no alternative login, use Horde login as baseurl
+            $baseUrl = $this->registry->getServiceLink('login');
+        };
+        
+        $redirectUrl = (string) Horde::Url($baseUrl, true)->add('url', $signedRequestUrl);
+
+        return $this->responseFactory->createResponse(302)->withHeader('Location', $redirectUrl);
     }
 }
