2ad40e7b56e58035b651951969e7bd2841906bc1
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-19 15:42:57 +0200

    feat: Patch prototype.js downstream to emit native events in parallel to prototype events when using .fire()

---
+++ src/Service/Exception/OAuthTokenNotFoundException.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2026 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @category Horde
+ * @package  Core
+ * @author   Ralf Lang <ralf.lang@ralf-lang.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ */
+
+namespace Horde\Core\Service\Exception;
+
+use RuntimeException;
+
+/**
+ * Exception thrown when no OAuth tokens exist for a user/provider pair
+ *
+ * @category Horde
+ * @package  Core
+ * @author   Ralf Lang <ralf.lang@ralf-lang.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ */
+class OAuthTokenNotFoundException extends RuntimeException {}
---
+++ src/Service/NullOAuthTokenService.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2026 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @category Horde
+ * @package  Core
+ * @author   Ralf Lang <ralf.lang@ralf-lang.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ */
+
+namespace Horde\Core\Service;
+
+use Horde\Core\Service\Exception\OAuthTokenNotFoundException;
+use Horde\Oauth\Client\TokenSet;
+
+/**
+ * Null OAuth token service — safe default when OAuth is not configured.
+ *
+ * Store is silently discarded. Any retrieval throws OAuthTokenNotFoundException.
+ *
+ * @category Horde
+ * @package  Core
+ * @author   Ralf Lang <ralf.lang@ralf-lang.de>
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ */
+class NullOAuthTokenService implements OAuthTokenService
+{
+    public function getAccessToken(string $userId, string $providerId): string
+    {
+        throw new OAuthTokenNotFoundException(
+            "No OAuth tokens for user '{$userId}' / provider '{$providerId}'"
+        );
+    }
+
+    public function store(string $userId, string $providerId, TokenSet $tokens): void
+    {
+    }
+
+    public function hasTokens(string $userId, string $providerId): bool
+    {
+        return false;
+    }
+
+    public function remove(string $userId, string $providerId): void
+    {
+    }
+
+    public function getTokenSet(string $userId, string $providerId): TokenSet
+    {
+        throw new OAuthTokenNotFoundException(
+            "No OAuth tokens for user '{$userId}' / provider '{$providerId}'"
+        );
+    }
+}
