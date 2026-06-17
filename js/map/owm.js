1442acd4429e15abc86849f3b367b68f166457e7
Torben Dannhauer <torben@dannhauer.de>
2026-06-17 09:56:38 +0200

    Update js/map/owm.js
    Co-authored-by: Ralf Lang <ralf.lang@ralf-lang.de>

--- js/map/owm.js
+++ js/map/owm.js
@@ -21,6 +21,7 @@ HordeMap.Owm = Class.create(
                 'streets': new OpenLayers.Layer.OSM(
                     'OpenStreetMap (Mapnik)',
                     [
+                      // TODO: Do not hardcode the URI (or allow an override variable with fallback to hardcode)
                         'https://tile.openstreetmap.org/${z}/${x}/${y}.png'
                     ],
                     { 'minZoomLevel': 1, 'numZoomLevels': 18 }
