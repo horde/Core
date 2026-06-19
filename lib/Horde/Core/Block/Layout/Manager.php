aaa78f93c6770424ec541afe5236644c2258d4d0
Torben Dannhauer <torben@dannhauer.de>
2026-06-19 11:57:35 +0200

    fix(core): validate portal layout CSRF with session checkToken
    Use Horde_Session::checkToken() for portal block layout saves so the
    token matches the value generated by getToken() in the edit form.
    Drop the Token injector path that rejected valid submissions.

--- lib/Horde/Core/Block/Layout/Manager.php
+++ lib/Horde/Core/Block/Layout/Manager.php
@@ -1,8 +1,5 @@
 <?php
 
-use Horde\Core\Session\HordeSession;
-use Horde\Token\Exception\TokenException;
-use Horde\Token\Token;
 use Horde\Util\Util;
 
 /**
@@ -218,19 +215,8 @@ class Horde_Core_Block_Layout_Manager extends Horde_Core_Block_Layout implements
             case 'save':
                 // Save the changes made to a block and continue editing.
             case 'save-resume':
-                // Check form token.
-                $tokenService = $GLOBALS['injector']->getInstance(Token::class);
-                try {
-                    $valid = $tokenService->isValid(
-                        (string) Util::getFormData('token'),
-                        HordeSession::CSRF_SEED
-                    );
-                } catch (TokenException $e) {
-                    throw new Horde_Exception('Invalid token!');
-                }
-                if (!$valid) {
-                    throw new Horde_Exception('Invalid token!');
-                }
+                // Check form token (same path as Horde_Session::getToken()).
+                $GLOBALS['session']->checkToken((string) Util::getFormData('token'));
 
                 // Get requested block type.
                 [$newapp, $newtype] = explode(':', Util::getFormData('app'));
